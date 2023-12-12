f8f0f7dd07c8bdb5c8f4cec080cf1eabe6b78cc3
陈俊飞 <chenjunfei@tuyoogame.com>
2023-12-12 18:33:02 +0800

    代码更新

--- panelObjs/storePanel.py
+++ panelObjs/storePanel.py
@@ -4,10 +4,10 @@ from tools.commonTools import *
 
 class RechargeStorePanel(BasePage):
     def click_btn_close(self):
-        self.click_element(element_data=ElementsData.RechargeStore.btn_close)
+        self.click_element(element_data=ElementsData.Store.btn_close)
         if RechargeStorePanel.is_panel_active(self):
             raise FindElementError
     def is_panel_active(self):
-        if self.exist(element_data=ElementsData.RechargeStore.RechargeStorePanel):
+        if self.exist(element_data=ElementsData.Store.StorePanel):
             return True
         return False
