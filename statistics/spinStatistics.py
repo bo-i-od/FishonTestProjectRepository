0afdc2e7bfd309215abab8d37ba9b38ae46e91f2
陈俊飞 <chenjunfei@tuyoogame.com>
2024-07-23 16:12:30 +0800

    共建转盘概率

--- statistics/spinStatistics.py
+++ statistics/spinStatistics.py
@@ -1,11 +1,26 @@
 from load_log import load_log
 
+import re
+
+# 自定义排序函数，以获取数字进行排序
+def extract_number(text):
+    if text.isdigit():
+        return int(text)
+    else:
+        # 提取所有数字部分并转为整数，如果键中包含非数字部分，则保持不变，适合更复杂场景
+        numbers = re.findall(r'\d+', text)
+        return int(numbers[0]) if numbers else text
+
+
+
+
+
 # 读取log
 data = load_log('spin_log.txt')
 
 
 # 索引转成分数
-index2point = {'1': '40', '2': '300', '3': '80_1', '4': '200_zhuli', '5': '60', '6': '200', '7': '100', '8': '200_free', '9': '80_2', '10': '1000'}
+index2point = {'1': '40', '2': '300', '3': '80_1', '4': '200_partner', '5': '60', '6': '200', '7': '100', '8': '200_free', '9': '80_2', '10': '1000'}
 
 # 统计次数
 data_len = len(data)
@@ -19,15 +34,16 @@ while cur < data_len:
         continue
     result_times[spinIndex] = 1
     cur += 1
-print(result_times)
 
 # 转成百分比结果
 result_percentage = {}
 for r in result_times:
     result_percentage[index2point[r]] = format(result_times[r] / data_len, ".2%")
 
+# 按键的数值部分排序并生成新字典
+sorted_dict = dict(sorted(result_percentage.items(), key=lambda item: extract_number(item[0])))
 print(f"共计进行{data_len}次实验")
-print(result_percentage)
+print(sorted_dict)
 
 
 
