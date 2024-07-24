b9ff763e656f0ac08a469ada858e447e431199f2
dengzhilei <707304943@qq.com>
2024-07-24 15:56:20 +0800

    update

--- statistics/fishStatistics.py
+++ statistics/fishStatistics.py
@@ -32,7 +32,6 @@ def print_list(target_list,title=""):
     print("=====",title,"=====")
     for i in target_list:
         print(i)
-# 仅限 非洪湖钓点，洪湖钓点id对不上有问题！！！！
 print("钓鱼次数：",len(numbers))
 print_list(boss_num,"rare,elite,monster 掉落数量和时机")
 print_list(yugu_num,"elite、monster鱼骨 掉落数量和时机")
