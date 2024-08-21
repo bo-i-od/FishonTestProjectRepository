bfccd6a4c4e869b60ee952ec3fafb529ed8aeb0f
dengzhilei <707304943@qq.com>
2024-08-21 20:36:29 +0800

    update

--- statistics/fishStarStatistics.py
+++ statistics/fishStarStatistics.py
@@ -5,10 +5,11 @@ data = load_log('hook_log.txt')
 # ------新增鱼获星数分布----------
 star_dict={}
 for i in data:
-    star=int(i['star'])
-    if star>0:
-        star_dict.setdefault(star,0)
-        star_dict[star]+=1
+    if i['star']:
+        star=int(i['star'])
+        if star>0:
+            star_dict.setdefault(star,0)
+            star_dict[star]+=1
 sort_list=sorted(star_dict.items(),key=lambda item:item[0],reverse=True)
 print("钓鱼星数统计")
 print(sort_list)
