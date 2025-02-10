d871c904d552236f4eb1ba77ea50ad8fe4d0b034
chenjunfei <269825635@qq.com>
2025-02-10 14:30:50 +0800

    关卡测试

--- test/test12.py
+++ test/test12.py
@@ -4,6 +4,7 @@ from scripts import battleTest
 if __name__ == '__main__':
     bp = BasePage(is_mobile_device=False, serial_number="127.0.0.1:21583")
     bp.quick_qte = True
+    bp.custom_cmd("setQuickQTE 1")
     battleTest.circulate_fish(bp)
 
     bp.connect_close()
