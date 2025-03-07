291fff7ec272bc11b10dc1784e8b101dab1000e5
lkl-lu <18776402026@163.com>
2025-03-07 16:43:18 +0800

    爬塔测试代码调整

--- panelObjs/RogueResultPanel.py
+++ panelObjs/RogueResultPanel.py
@@ -7,4 +7,24 @@ class RogueResultPanel(BasePage):
         return self.exist(element_data=ElementsData.RogueResultPanel.RogueResultPanel)
 
     def click_btn_orange(self):
-        self.click_element(element_data=ElementsData.RogueResultPanel.btn_orange)
+        self.click_until_disappear(element_data=ElementsData.RogueResultPanel.btn_orange)
+
+    def result(self,gear_lv, kind):
+        res = f"{gear_lv}级_"
+        if kind == 1:
+            res += "力_"
+        elif kind == 2:
+            res += "敏_"
+        elif kind == 3:
+            res += "智_"
+        if self.exist(element_data=ElementsData.RogueResultPanel.panel_succeed):
+            value = self.get_text(element_data=ElementsData.RogueResultPanel.succeed_item2_value)
+            res += f"难度{value}成功_"
+            if self.exist(element_data=ElementsData.RogueResultPanel.tips):
+                res += "无损"
+            else:
+                res += "非无损"
+        elif self.exist(element_data=ElementsData.RogueResultPanel.panel_lost):
+            value = self.get_text(element_data=ElementsData.RogueResultPanel.lost_item2_value)
+            res += f"难度{value}失败"
+        print(res)
--- panelObjs/RogueSelectSkillPanel.py
+++ panelObjs/RogueSelectSkillPanel.py
@@ -1,3 +1,5 @@
+import random
+
 from common.basePage import BasePage
 from configs.elementsData import ElementsData
 
@@ -15,6 +17,47 @@ class RogueSelectSkillPanel(BasePage):
     def select_skill(self, index=-1):
         self.click_object_of_plural_objects(element_data=ElementsData.RogueSelectSkillPanel.skill_list, index=index, ignore_set={"RogueSelectSkillPanel"})
 
+    def choose_skill(self):
+        self.wait_for_appear(element_data=ElementsData.RogueSelectSkillPanel.skill_attribute_list, interval=0.5,timeout=10)
+        skill_list = self.get_icon_list(element_data=ElementsData.RogueSelectSkillPanel.skill_attribute_list)
+        skill_attribute_list = [int(s.split("_")[-1]) for s in skill_list]
+        # print(skill_list)
+        # print(skill_attribute_list)
+        # 选择最强的技能组成新列表
+        best_attribute = min(skill_attribute_list)
+        best_attribute_list = [i for i, x in enumerate(skill_attribute_list) if x == best_attribute]
+        # 随机选择一个下标输出
+        index = random.choice(best_attribute_list)
+        skill_position_list = self.get_position_list(element_data=ElementsData.RogueSelectSkillPanel.skill_list)
+        self.click_position(skill_position_list[index])
+        # print(f"选择第{index + 1}个技能")
+        self.click_element(element_data=ElementsData.RogueSelectSkillPanel.btn_orange)
+
+    def choose_challenge(self,angry_target, angry_now):
+        challenge_list = self.get_text_list(element_data=ElementsData.RogueSelectSkillPanel.challenge_list)
+        challenge_list = [int(s) for s in challenge_list]
+        choose_num = 0
+        # 选择怒气值最高/低的事件
+        max_challenge = max(challenge_list)
+        if (angry_target - angry_now) > max_challenge:
+            choose_num = max_challenge
+        else:
+            choose_num = angry_target - angry_now
+        index = challenge_list.index(choose_num)
+
+        challenge_position_list = self.get_position_list(element_data=ElementsData.RogueSelectSkillPanel.challenge_position_list)
+        self.click_position(position=challenge_position_list[index])
+
+        # print(f"已选择第{index+1}个事件")
+        self.ray_input(kind='click',element_data=ElementsData.RogueSelectSkillPanel.btn_orange)
+
+    def get_now_num(self):
+        self.ray_input(kind='click',element_data=ElementsData.RogueSelectSkillPanel.btn_blue)
+        num = self.get_text(element_data=ElementsData.RogueShowSkillsPanel.raise_all)
+        num = int(num)
+        self.click_element(element_data=ElementsData.RogueShowSkillsPanel.btn_close)
+        return num
+
 
 
 if __name__ == "__main__":
@@ -22,3 +65,5 @@ if __name__ == "__main__":
     RogueSelectSkillPanel.select_skill(bp)
     RogueSelectSkillPanel.click_btn_orange(bp)
     bp.connect_close()
+
+
