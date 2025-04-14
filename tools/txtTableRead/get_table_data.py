c369a6af844e4eb7518dc80faf622614ca53c630
dengzhilei <707304943@qq.com>
2025-04-14 16:26:27 +0800

    补充注释

--- tools/txtTableRead/get_table_data.py
+++ tools/txtTableRead/get_table_data.py
@@ -6,8 +6,18 @@ from tools.txtTableRead.translate_assist_diff_versions import doWorkOnDataTxtFil
 
 file_path = os.path.dirname(os.path.dirname(EXCEL_PATH))+'/ElementData/BaseData/'
 
+
 def get_table_data(data_key):
-    # data_key = "BATTLE_SKILL"
+    """
+    data_key: 表名，不带后缀
+    返回值: {
+        key1:value1,
+        key2:value1,
+        ...
+    }
+    key: 第一列的id（非tpid)
+    value: 每一行的数据，同样以key-value的形式保存，key为表头字段
+    """
     collect_list = []
     collect_map = {}
     collect_dup_map = None
@@ -15,7 +25,10 @@ def get_table_data(data_key):
     return collect_map[data_key.lower()]
 
 def get_table_list_data(data_keys):
-    # data_keys = ["BATTLE_SKILL", "BATTLE_BUFF", "FISH_TYPECLASS_VISUAL", "FISH_STAR_GRADING", "FISHERIES", "FISH"]
+    """
+    data_keys = ["BATTLE_SKILL", "BATTLE_BUFF", "FISH_TYPECLASS_VISUAL", "FISH_STAR_GRADING", "FISHERIES", "FISH"]
+    返回值也是多套一层list
+    """
     collect_list = []
     collect_map = {}
     collect_dup_map = None
@@ -28,6 +41,10 @@ def get_table_list_data(data_keys):
     return result
 
 def write_table_data(table_name,table_data):
+    """
+    table_name: 表名，不带后缀
+    table_data: 和上文 get_table_data 一致的数据结构
+    """
     doWriteConfigTable(file_path, table_name, table_data.values())
 
 if __name__ == '__main__':
