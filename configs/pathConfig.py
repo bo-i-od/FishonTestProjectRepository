224de51b5c19da2c94c3d51a4d04b3d350aa0e62
dengzhilei <707304943@qq.com>
2025-06-26 18:38:35 +0800

    update release path

--- configs/pathConfig.py
+++ configs/pathConfig.py
@@ -1,2 +1,3 @@
 EXCEL_PATH = r"C:/Dev/datapool/策划模板导出工具/"
 thrift2py_folder_path = r'C:/trunkCHS/tools/gen-message/messages/'
+RELEASE_EXCEL_PATH = r"C:/Users/TU/Documents/release_2025_4_1/datapool/策划模板导出工具/"
