a6a243f3d40db343f0c2b8e01e66ea93f2f84562
dengzhilei <707304943@qq.com>
2025-06-26 19:56:11 +0800

    update

--- activities/zhilei_config/timer_main_check.py
+++ activities/zhilei_config/timer_main_check.py
@@ -1,14 +1,25 @@
-from tools.txtTableRead.get_table_data import get_table_data,write_table_data
+from tools.txtTableRead.get_table_data import get_table_data, write_table_data, get_table_data_by_file
 import copy
 from datetime import datetime,timedelta
+from configs.pathConfig import EXCEL_PATH,RELEASE_EXCEL_PATH
+import os
 
 # 显示活动时间范围
 show_start_data="2025-07-11 00:00:00"
 show_end_data="2025-08-07 00:00:00"
 
+SORT_OPEN_TIME=1
+SORT_END_TIME=2
+
+sort_type=SORT_END_TIME  # 排序方式
+
 # timer数据
 data_keys = "timer_main"
-timer_data=get_table_data(data_keys)
+dev_path = os.path.dirname(os.path.dirname(EXCEL_PATH))+'/ElementData/BaseData/'+data_keys.upper()+".data.txt"
+release_path = os.path.dirname(os.path.dirname(EXCEL_PATH))+'/ElementData/BaseData/'+data_keys.upper()+".data.txt"
+
+# 这里用dev还是release
+timer_data=get_table_data_by_file(data_keys,release_path)
 
 # 时间数据格式化
 date_format = "%Y-%m-%d %H:%M:%S"
@@ -37,7 +48,8 @@ for key,value in timer_data.items():
             result_list.append([value['name'],openTime,endTime,delta_day,key])
     except:
         pass
+
 # 按时间先后排序
-new_list=sorted(result_list,key=lambda x:x[2])
+new_list=sorted(result_list,key=lambda x:x[sort_type])
 for i in new_list:
     print(i[4],"  ",i[1],"  ",i[2],"    ",i[3],"   ",i[0])
